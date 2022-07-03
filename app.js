75d2c8d2c29968f196a5421cbc40215ffa2dafb6
S4cr4b011 <ajuans@miumg.edu.gt>
2022-07-03 04:43:30 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let sound = new Audio('./audio/Bryan Adams - Everything I Do I Do For You.mp3');
+playBtn.addEventListener('click', ()=>{
+    sound.play();
+});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
